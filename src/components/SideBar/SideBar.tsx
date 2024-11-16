abc30f6f8e92d28d16da1960cd6d4dc213ee005e
Varun Patel <118429904+varunpatel0704@users.noreply.github.com>
2024-11-16 17:07:48 +0530

    add sidebar skeletal markup and css

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,24 +1,88 @@
-import star from '../../assets/star.svg';
-import clock from '../../assets/clock.svg'
-import add from '../../assets/add.svg';
-import compose from '../../assets/compose.svg';
-import inbox from '../../assets/inbox.svg';
-import send from '../../assets/send.svg';
-import arrowDown from '../../assets/arrow_down.svg';
-function SideBar() {
+import star from "../../assets/star.svg";
+import clock from "../../assets/clock.svg";
+import add from "../../assets/add.svg";
+import compose from "../../assets/compose.svg";
+import inbox from "../../assets/inbox.svg";
+import send from "../../assets/send.svg";
+import arrowDown from "../../assets/arrow_down.svg";
+import "./SideBar.css";
+
+function NavListItem({
+  visible,
+  src,
+  title,
+  options,
+}: {
+  visible: boolean;
+  src: string;
+  title: string;
+  options?: navListItem["options"];
+}) {
   return (
-    <main>
-      <ul>
-        <li><img src={compose} alt="" /></li>
-        <li><img src={inbox} alt="" /></li>
-        <li><img src={star} alt="" /></li>
-        <li><img src={clock} alt="" /></li>
-        <li><img src={send} alt="" /></li>
-        <li><img src={arrowDown} alt="" /></li>
-        <li><img src={add} alt="" /></li>
-      </ul>
-    </main>
-  )
+    <li className="nav-li">
+      <img src={src} alt={title} />
+      <div className={`${visible?'show':'hide'}`}>
+        <span>{title}</span>
+        {options?.hasValue && <span> {options.value} </span>}
+      </div>
+    </li>
+  );
 }
 
-export default SideBar
+interface navListItem {
+  src: string;
+  title: string;
+  options?: { hasValue: boolean; value: number };
+}
+const navListItems: navListItem[] = [
+  {
+    title: "Compose",
+    src: compose,
+  },
+  {
+    title: "Inbox",
+    src: inbox,
+    options: {
+      hasValue: true,
+      value: 4152,
+    },
+  },
+  {
+    title: "Starred",
+    src: star,
+  },
+  {
+    title: "Snoozed",
+    src: clock,
+  },
+  {
+    title: "Sent",
+    src: send,
+  },
+  {
+    title: "More",
+    src: arrowDown,
+  },
+];
+
+function SideBar() {
+  const visible = false;
+
+  return (
+    <nav className="navigation">
+      <ul className="">
+        {navListItems.map(({ src, title, options }) => (
+          <NavListItem
+            key={title}
+            src={src}
+            title={title}
+            visible={visible}
+            options={options}
+          />
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default SideBar;
